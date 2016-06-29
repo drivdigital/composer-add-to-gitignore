38559b9b0c7bb3219682c4c9ddee14b06578f113
Eivin Landa <landa@drivdigital.no>
2016-06-29 15:07:42 +0200

    Implemented add-to-gitignore

--- src/plugin.php
+++ src/plugin.php
@@ -3,17 +3,11 @@ namespace drivdigital;
 
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
-use Composer\Installer\InstallerEvent;
-use Composer\Installer\InstallerEvents;
 use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
-use Composer\Plugin\CommandEvent;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PreFileDownloadEvent;
-use Composer\Script\Event;
-use Composer\Script\ScriptEvents;
 
 class plugin implements PluginInterface, EventSubscriberInterface {
   /** @type Composer */
@@ -30,74 +24,37 @@ class plugin implements PluginInterface, EventSubscriberInterface {
 
   /**
   * Returns an array of event names this subscriber wants to listen to.
-  *
-  * The array keys are event names and the value can be:
-  *
-  * * The method name to call. Then the priority defaults to 0.
-  * * An array composed of the method name to call and the priority.
-  * * An array of arrays composed of the method names to call and respective
-  * priorities, or 0 if unset.
-  *
-  * Examples:
-  *
-  * * array( 'eventName' => 'methodName' )
-  * * array( 'eventName' => array( 'methodName', $priority ) )
-  * * array( 'eventName' => array( array( 'methodName1', $priority ), array( 'methodName2' ) )
-  *
   * @return array The event names to listen to
   */
   public static function getSubscribedEvents() {
     return [
-    PluginEvents::COMMAND => 'onCommand',
-    PluginEvents::PRE_FILE_DOWNLOAD => 'onPreFileDownload',
-    ScriptEvents::PRE_UPDATE_CMD => 'onPreUpdateCmd',
-    ScriptEvents::POST_UPDATE_CMD => 'onPostUpdateCmd',
-    InstallerEvents::PRE_DEPENDENCIES_SOLVING => 'onPreDependenciesSolving',
-    InstallerEvents::POST_DEPENDENCIES_SOLVING => 'onPostDependenciesSolving',
-    PackageEvents::PRE_PACKAGE_UPDATE => 'onPrePackageUpdate',
-    PackageEvents::POST_PACKAGE_UPDATE => 'onPostPackageUpdate',
-    PackageEvents::PRE_PACKAGE_INSTALL => 'onPrePackageInstall',
-    PackageEvents::POST_PACKAGE_INSTALL => 'onPostPackageInstall',
+      PackageEvents::POST_PACKAGE_UPDATE => 'onPostPackageUpdate',
+      PackageEvents::POST_PACKAGE_INSTALL => 'onPostPackageInstall',
     ];
   }
-
-  public function onCommand( CommandEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
-  public function onPreFileDownload( PreFileDownloadEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
-  public function onPreUpdateCmd( Event $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
-  public function onPostUpdateCmd( Event $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
-  public function onPrePackageUpdate( PackageEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
   public function onPostPackageUpdate( PackageEvent $event ) {
     $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
+    $this->add_package_to_gitignore( $event );
   }
-
-  public function onPrePackageInstall( PackageEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
   public function onPostPackageInstall( PackageEvent $event ) {
     $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
+    $this->add_package_to_gitignore( $event );
   }
+  public function add_package_to_gitignore( $event ) {
+    $package = $event->getOperation()->getPackage();
+    $installationManager = $event->getComposer()->getInstallationManager();
+    $dir = $installationManager->getInstallPath($package);
+    if ( strpos( $dir, 'vendor' ) === 0 )
+      return;
+    $content = '';
+    if ( file_exists( '.gitignore' ) ) {
+      $content = file_get_contents( '.gitignore' );
+    }
+    if ( false !== strpos( $content, $dir ) )
+      return;
 
-  public function onPreDependenciesSolving( InstallerEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
-  }
-
-  public function onPostDependenciesSolving( InstallerEvent $event ) {
-    $this->io->write( __CLASS__ . '::' . __METHOD__ . "()" );
+    $content =  trim( $content ) ."\n/$dir\n";
+    file_put_contents( '.gitignore', $content );
+    $this->io->write( "\033[1;35mAdded \"$dir\" to .gitignore\033[0m\n" );
   }
 }
